dc1a09be0e6b58c23db78cc634da59e8e4e4bcd9
lvitters <luvit023@gmail.com>
2024-11-21 15:20:58 +0100

    not sure what is happening, grid lines are gone, and now it stops sometimes?

--- sketch.js
+++ sketch.js
@@ -2,16 +2,14 @@
 let canvasWidth = 400;
 let canvasHeight = 400;
 
-let gridLinesX;
-let gridLinesY;
+let gridLines
 
 function setup() {
 	createCanvas(canvasWidth, canvasHeight);
 	frameRate(30);
 	pixelDensity(1);
 
-	gridLinesX = new NoiseObject(Math.random() * 100, .1);
-	gridLinesY = new NoiseObject(Math.random() * 100, .1);
+	gridLines = new NoiseObject(Math.random() * 100, .1);
 
 	//get pixel array for manipulation
 	loadPixels();
@@ -19,11 +17,13 @@ function setup() {
 
 function draw() {
 
-	//console.log(computeGridLines().x + " " + computeGridLines().y);
+	//get gridLines
+	let gridLines = computeGridLines();
+	console.log(gridLines);
 
 	//manipulate pixel array
-	for (let x = 0; x < canvasWidth; x += computeGridLines().x) {
-		for (let y = 0; y < canvasHeight; y += computeGridLines().y) {
+	for (let x = 0; x < canvasWidth; x += gridLines) {
+		for (let y = 0; y < canvasHeight; y += gridLines) {
 			//get index in array from coordinates
 			let index = (x + y * canvasWidth) * 4;
 			pixels[index + 0] = Math.random() * 255;		//red
@@ -37,11 +37,10 @@ function draw() {
 	updatePixels();
 }
 
-//compute grid lines (every x columns and every y rows) to apply to pixel array manipulation
+//compute grid lines to apply to pixel array manipulation, weighted
 function computeGridLines() {
-	let x = floor(map(gridLinesX.getNoise(), 0, 1, -128, 64));
-	let y = floor(map(gridLinesY.getNoise(), 0, 1, -128, 64));
-	if (x < 1) x = 1;
-	if (y < 1) y = 1;
-	return {x, y};
+	let x = floor(map(gridLines.getNoise(), 0, 1, -5, 20));
+	if (x < 1) x = 1;	//don't go under one
+	else if (x > 1) x *= 2;		//keep number even
+	return x;
 }

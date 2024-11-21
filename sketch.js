5febdb2892367079ad68a760e33eb591d5c4ec9d
lvitters <luvit023@gmail.com>
2024-11-21 13:46:42 +0100

    change canvas size via vss

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
-let canvasWidth = 800;
-let canvasHeight = 800;
+//res of canvas, change size in index.html
+let canvasWidth = 400;
+let canvasHeight = 400;
+
+let resMutiplierT = 0;
 
 function setup() {
 	createCanvas(canvasWidth, canvasHeight);
 	frameRate(30);
+
+	//get pixel array for manipulation
+	loadPixels();
 }
 
 function draw() {
-	//get pixel array for manipulation
-	loadPixels();
 
 		//every pixel has 4 values, and 4 indices in the pixels array
 		for  (let i = 0; i < pixels.length; i += 4) {
@@ -20,4 +24,10 @@ function draw() {
 
 	//write to pixels array
 	updatePixels();
+}
+
+//get noise
+function noisyResMultiplier() {
+	resMutiplierT += .1;
+	return noise(resMutiplierT);
 }
